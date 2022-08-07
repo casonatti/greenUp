00d7f39890a5c39ba1487b871bb6108d54a71229
casonatti <jeison.casonatti@gmail.com>
2022-08-06 22:16:54 -0300

    communication fix

--- main.cpp
+++ main.cpp
@@ -68,9 +68,11 @@ static void * thr_participant_function(void* arg) {
     int sockfd, ret_value;
     int true_flag = true;
     char buffer[BUFFER_SIZE] = {0};
-    struct sockaddr_in recv_addr, serv_addr;
-    struct sockaddr_in sockaddr;
+    struct sockaddr_in recv_addr, serv_addr, from;
+    struct hostent *server;
     
+    server = gethostbyname("localhost"); //TODO modificar
+
     //creates the socket
     if((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
             cout << "Socket creation error." << endl;
@@ -92,18 +94,18 @@ static void * thr_participant_function(void* arg) {
     }
 
     //participant receiving address configuration
-    socklen_t recv_addr_len = sizeof(recv_addr);
+    unsigned int serv_addr_len;
+    
     memset(&recv_addr, 0, sizeof recv_addr);
     recv_addr.sin_family = AF_INET;
     recv_addr.sin_port = (in_port_t) htons(PORT_PARTICIPANT_LISTENING);
-    // recv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  //important for broadcast listening
-    inet_aton("127.255.255.255", &recv_addr.sin_addr); //broadcast address for UNIX
+    recv_addr.sin_addr.s_addr = htonl(INADDR_ANY);  //important for broadcast listening
 
-    //participant sending address configuration
-    // memset(&serv_addr, 0, sizeof serv_addr);
-    // serv_addr.sin_family = AF_INET;
-    // serv_addr.sin_port = htons(PORT_MANAGER_LISTENING);
-    // serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    //manager address configuration
+    memset(&serv_addr, 0, sizeof recv_addr);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = (in_port_t) htons(PORT_MANAGER_LISTENING);
+    serv_addr.sin_addr = *((struct in_addr *) server->h_addr);
 
     // bind the participant's listening port
     ret_value = bind(sockfd, (struct sockaddr*) &recv_addr, sizeof(recv_addr));
@@ -116,7 +118,7 @@ static void * thr_participant_function(void* arg) {
         //wait for manager's message
         cout << "\nTo aguardando msg..." << endl; //TODO debug (apagar depois)
         memset(buffer, '\0', BUFFER_SIZE);
-        ret_value = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&recv_addr, &recv_addr_len);
+        ret_value = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr*)&from, &serv_addr_len);
         if(ret_value < 0) {
             cout << "Recvfrom error." << endl; //TODO tratar esse erro!
             exit(0);
@@ -129,8 +131,8 @@ static void * thr_participant_function(void* arg) {
         if(strcmp(buffer, SLEEP_SERVICE_DISCOVERY) == 0) {
             //TODO criar thread Discovery Subservice (?)
             strcpy(buffer, "resposta participante!");
-            cout << "Enviando mensagem: " << buffer << endl; 
-            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*) &recv_addr, sizeof recv_addr);
+            cout << "Enviando mensagem: " << buffer << endl;
+            sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*) &serv_addr, sizeof serv_addr);
         }
 
         if(strcmp(buffer, SLEEP_STATUS_REQUEST) == 0) {
@@ -175,7 +177,8 @@ int main(int argc, char** argv) {
         int sockfd;
         int true_flag = true;
         char buffer[BUFFER_SIZE] = {0};
-        struct sockaddr_in send_addr, recv_addr;
+        socklen_t participant_len;
+        struct sockaddr_in manager_addr, broadcast_addr, participant_addr;
         managerDB manDb[MAX_MACHINES]; //structure hold by manager
 
         if(strcmp(argv[1], "manager") != 0) { //argv[1] != "manager"
@@ -202,19 +205,18 @@ int main(int argc, char** argv) {
             exit(0);
         }
 
-        //configure manager's sending and receiving addresses
-        memset(&send_addr, 0, sizeof send_addr);
-        send_addr.sin_family = AF_INET;
-        send_addr.sin_port = (in_port_t) htons(PORT_PARTICIPANT_LISTENING);
-        inet_aton("127.255.255.255", &send_addr.sin_addr);
+        //configure manager's sending and broadcast addresses
+        memset(&manager_addr, 0, sizeof manager_addr);
+        manager_addr.sin_family = AF_INET;
+        manager_addr.sin_port = (in_port_t) htons(PORT_MANAGER_LISTENING);
+        manager_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-        memset(&recv_addr, 0, sizeof recv_addr);
-        // recv_addr.sin_family = AF_INET;
-        // recv_addr.sin_port = (in_port_t) htons(PORT_MANAGER_LISTENING);
-        // recv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        memset(&broadcast_addr, 0, sizeof broadcast_addr);
+        broadcast_addr.sin_family = AF_INET;
+        broadcast_addr.sin_port = (in_port_t) htons(PORT_PARTICIPANT_LISTENING);
+        inet_aton("127.255.255.255", &broadcast_addr.sin_addr);
 
-        //TODO Descomentar essa parte pro Manager poder "ouvir" as respostas!
-        ret_value = bind(sockfd, (struct sockaddr *) &recv_addr, sizeof recv_addr);
+        ret_value = bind(sockfd, (struct sockaddr *) &manager_addr, sizeof manager_addr);
         if(ret_value < 0) {
             cout << "Bind socket error." << endl;
             exit(0);
@@ -223,14 +225,14 @@ int main(int argc, char** argv) {
         //TODO debug (apagar depois)
         int i = 0;
 
-        //TODO teste (apagar depois)
-        socklen_t send_addr_len = sizeof(send_addr);
+        //TODO teste...
+        participant_len = sizeof(struct sockaddr_in);
 
         while(true) {
             memset(buffer, '\0', BUFFER_SIZE);
-            inet_aton("127.255.255.255", &send_addr.sin_addr);
+            //inet_aton("127.255.255.255", &broadcast_addr.sin_addr);
             //sending in broadcast
-            ret_value = sendto(sockfd, SLEEP_SERVICE_DISCOVERY, strlen(SLEEP_SERVICE_DISCOVERY), 0, (struct sockaddr *) &send_addr, sizeof send_addr);
+            ret_value = sendto(sockfd, SLEEP_SERVICE_DISCOVERY, strlen(SLEEP_SERVICE_DISCOVERY), 0, (struct sockaddr *) &broadcast_addr, sizeof broadcast_addr);
             if(ret_value < 0) {
                 cout << "Sendto error." << endl;
                 exit(0);
@@ -241,7 +243,7 @@ int main(int argc, char** argv) {
 
             memset(buffer, '\0', BUFFER_SIZE);
             // ret_value = recv(sockfd, buffer, sizeof(buffer)-1, 0);
-            ret_value = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &send_addr, &send_addr_len);
+            ret_value = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, (struct sockaddr *) &participant_addr, &participant_len);
             if(ret_value < 0) {
                 cout << "Recvfrom error.";
                 exit(0);
